92991efd75834f2a9e1aa9c98380c009e164f2bc
Brenek Harrison <brenekharrison@gmail.com>
2023-11-15 16:45:45 -0700

    Run auto-editor as usual if arguments are passed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use std::process::Command;
+use std::env;
+use std::process::{Command, ExitCode};
 
-fn main() {
-    Command::new("auto-editor")
-        .arg("--player")
-        .arg("/bin/true")
-        .arg("/home/brenekh/Videos/CS321-Intro-Video.mkv")
-        .status()
-        .expect("auto-editor process failed to execute");
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 1 {
+        let mut cmd = Command::new("auto-editor");
+        cmd.arg("--player").arg("/bin/true");
+
+        for arg in args {
+            cmd.arg(arg);
+        }
+
+        let status = cmd.status().expect("auto-editor process failed to execute");
+        return (status.code().unwrap_or(127) as u8).into();
+    }
+
+    println!("TODO: Run as watcher daemon");
+
+    ExitCode::SUCCESS
 }
