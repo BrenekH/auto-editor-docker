327147de81a5d59e14e83744125314c8f50348b0
Brenek Harrison <brenekharrison@gmail.com>
2023-11-15 21:06:55 -0700

    Process files once they have been closed in the watch directory

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,81 @@
 use std::env;
+use std::fs;
+use std::io;
 use std::path::Path;
 use std::process::{Command, ExitCode};
+use std::thread;
 
+use notify::Event;
+use notify::EventKind;
 use notify::RecursiveMode;
 use notify::Watcher;
 
+use notify::event::AccessKind;
+use notify::event::AccessMode;
 use path_clean::PathClean;
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 1 {
-        let mut cmd = Command::new("auto-editor");
-        cmd.arg("--player").arg("/bin/true");
-
-        for arg in args {
-            cmd.arg(arg);
-        }
-
-        let status = cmd.status().expect("auto-editor process failed to execute");
-        return (status.code().unwrap_or(127) as u8).into();
+        return (run_auto_editor(&args).expect("auto-editor process failed to execute") as u8)
+            .into();
     }
 
     // This unwrap is obscuring a potential error source
-    let watch_dir = env::var("AUTO_EDITOR_WATCH_DIR").unwrap_or("./".into());
+    let watch_dir = env::var("AUTO_EDITOR_WATCH_DIR").unwrap_or("./watch".into());
     let watch_dir = Path::new(&watch_dir).clean();
 
+    let output_dir = env::var("AUTO_EDITOR_OUTPUT_DIR").unwrap_or("./output".into());
+    let output_dir = Path::new(&output_dir).clean();
+
     if !watch_dir.exists() {
-        if let Err(e) = std::fs::create_dir_all(&watch_dir) {
+        if let Err(e) = fs::create_dir_all(&watch_dir) {
             println!("{e}");
             return ExitCode::FAILURE;
         }
     }
 
-    let watcher = notify::recommended_watcher(|res| match res {
+    if !output_dir.exists() {
+        if let Err(e) = fs::create_dir_all(&output_dir) {
+            println!("{e}");
+            return ExitCode::FAILURE;
+        }
+    }
+
+    let watcher = notify::recommended_watcher(move |res: Result<Event, notify::Error>| match res {
         Ok(event) => {
-            println!("{event:?}");
+            if event.kind == EventKind::Access(AccessKind::Close(AccessMode::Write)) {
+                for path_buf in event.paths {
+                    let out_dir = output_dir.clone();
+                    thread::spawn(move || {
+                        let path = Path::new(&path_buf);
+
+                        let proc_result = run_auto_editor(
+                            &(vec![
+                                path_buf.to_str().unwrap().into(),
+                                "--output-file".into(),
+                                out_dir
+                                    .join(path.file_name().unwrap())
+                                    .to_str()
+                                    .unwrap()
+                                    .into(),
+                            ]),
+                        );
+
+                        match proc_result {
+                            Ok(exit_code) => {
+                                if exit_code == 0 {
+                                    if let Err(e) = fs::remove_file(path_buf) {
+                                        println!("{e}")
+                                    }
+                                }
+                            },
+                            Err(e) => println!("{e}"),
+                        }
+                    });
+                }
+            }
         }
         Err(e) => {
             println!("{e}");
@@ -61,4 +101,16 @@ fn main() -> ExitCode {
     loop {
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
+}
+
+fn run_auto_editor(args: &[String]) -> io::Result<i32> {
+    let mut cmd = Command::new("auto-editor");
+    cmd.arg("--player").arg("/bin/true");
+
+    for arg in args {
+        cmd.arg(arg);
+    }
+
+    let status = cmd.status()?;
+    Ok(status.code().unwrap_or(127))
 }
