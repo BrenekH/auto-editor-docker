9c76b9d4b198a0d815fde550c9f89069fadf7ff8
Brenek Harrison <brenekharrison@gmail.com>
2023-11-15 17:50:34 -0700

    Print out events from the watch directory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::env;
+use std::path::Path;
 use std::process::{Command, ExitCode};
+
+use notify::RecursiveMode;
+use notify::Watcher;
+
+use path_clean::PathClean;
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
@@ -16,7 +22,43 @@ fn main() -> ExitCode {
         return (status.code().unwrap_or(127) as u8).into();
     }
 
-    println!("TODO: Run as watcher daemon");
+    // This unwrap is obscuring a potential error source
+    let watch_dir = env::var("AUTO_EDITOR_WATCH_DIR").unwrap_or("./".into());
+    let watch_dir = Path::new(&watch_dir).clean();
 
-    ExitCode::SUCCESS
+    if !watch_dir.exists() {
+        if let Err(e) = std::fs::create_dir_all(&watch_dir) {
+            println!("{e}");
+            return ExitCode::FAILURE;
+        }
+    }
+
+    let watcher = notify::recommended_watcher(|res| match res {
+        Ok(event) => {
+            println!("{event:?}");
+        }
+        Err(e) => {
+            println!("{e}");
+        }
+    });
+
+    let mut watcher = match watcher {
+        Ok(w) => w,
+        Err(e) => {
+            println!("{e}");
+            return ExitCode::FAILURE;
+        }
+    };
+
+    match watcher.watch(&watch_dir, RecursiveMode::Recursive) {
+        Err(e) => {
+            println!("{e}");
+            return ExitCode::FAILURE;
+        }
+        _ => {}
+    }
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
